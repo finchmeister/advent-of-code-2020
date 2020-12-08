762fb609c053918cf3d7dfff4f336744c6f04111
Jo Finch <thefinchmeister@gmail.com>
2020-12-08 11:24:45 +0000

    Add day 3, part 2

--- 03/03.go
+++ 03/03.go
@@ -7,14 +7,38 @@ import (
 	"strings"
 )
 
+type Directions struct {
+	right int
+	down  int
+}
+
 func FindNoOfTreesPt1(slope [][]string) int {
+	return FindNoOfTrees(slope, Directions{3, 1})
+}
+
+func FindTotalNoOfTreesPt2(slope [][]string, directions []Directions) int {
+	totalMultiple := 1
+	for _, direction := range directions {
+		totalMultiple = totalMultiple * FindNoOfTrees(slope, direction)
+	}
+
+	return totalMultiple
+}
+
+func FindNoOfTrees(slope [][]string, direction Directions) int {
 	width := len(slope[0])
 	noOfTrees := 0
+	i := 0
 	for y := range slope {
-		x := int(math.Mod(float64(y*3), float64(width)))
+		if int(math.Mod(float64(y), float64(direction.down))) != 0 {
+			continue
+		}
+
+		x := int(math.Mod(float64(i*direction.right), float64(width)))
 		if slope[y][x] == "#" {
 			noOfTrees++
 		}
+		i++
 	}
 
 	return noOfTrees
@@ -45,4 +69,12 @@ func main() {
 	slope := parseSlope(loadSlope())
 	fmt.Println("Pt1 No of Trees")
 	fmt.Println(FindNoOfTreesPt1(slope))
+	fmt.Println("Pt2 Total No of Trees")
+	fmt.Println(FindTotalNoOfTreesPt2(slope, []Directions{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}))
 }
--- 03/03_test.go
+++ 03/03_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 var testInput string = `..##.......
 #...#...#..
@@ -14,12 +16,58 @@ var testInput string = `..##.......
 #...##....#
 .#..#...#.#`
 
-func TestFindNumberOfTreesA(t *testing.T) {
+type Fixtures struct {
+	directions    Directions
+	expectedTrees int
+}
+
+func TestFindNumberOfTreesPt1(t *testing.T) {
 	testSlope := parseSlope(testInput)
 	got := FindNoOfTreesPt1(testSlope)
 	expected := 7
 
 	if got != expected {
 		t.Error("Fail")
+	}
+}
+
+func TestFindNumberOfTrees(t *testing.T) {
+	testSlope := parseSlope(testInput)
+	fixtures := []Fixtures{
+		{Directions{1, 1}, 2},
+		{Directions{3, 1}, 7},
+		{Directions{5, 1}, 3},
+		{Directions{7, 1}, 4},
+		{Directions{1, 2}, 2},
+	}
+
+	for _, fixture := range fixtures {
+		got := FindNoOfTrees(testSlope, fixture.directions)
+		expected := fixture.expectedTrees
+
+		if got != expected {
+			t.Errorf(
+				"Expected FindNoOfTreesPt2(right: %d, down: %d) = %d; want %d",
+				fixture.directions.right,
+				fixture.directions.down,
+				got,
+				fixture.expectedTrees)
+		}
+	}
+}
+
+func TestFindTotalNoOfTreesPt2(t *testing.T) {
+	testSlope := parseSlope(testInput)
+	got := FindTotalNoOfTreesPt2(testSlope, []Directions{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	})
+	expected := 336
+
+	if got != expected {
+		t.Errorf("Expected %d, got %d", expected, got)
 	}
 }
