6b038a4f86f3c1b04be90e36bdafe4d7178eb117
Jo Finch <thefinchmeister@gmail.com>
2020-12-08 17:57:51 +0000

    Add day 4, part 2

--- 04/04.go
+++ 04/04.go
@@ -4,10 +4,30 @@ import (
 	"fmt"
 	"io/ioutil"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 func FindNoOfValidPassportsPt1(passportRawData []string) int {
+	return validatePassports(passportRawData, validatorPt1)
+}
+
+func FindNoOfValidPassportsPt2(passportRawData []string) int {
+	return validatePassports(passportRawData, validatorPt2)
+}
+
+func validatePassports(passportRawData []string, validator func(passportRawRow string) bool) int {
+	i := 0
+	for _, passportRawRow := range passportRawData {
+		if validator(passportRawRow) {
+			i++
+		}
+	}
+
+	return i
+}
+
+func validatorPt1(passportRawRow string) bool {
 	requiredFields := []string{
 		"byr",
 		"iyr",
@@ -16,26 +36,135 @@ func FindNoOfValidPassportsPt1(passportRawData []string) int {
 		"hcl",
 		"ecl",
 		"pid",
-		//"cid",
 	}
 
-	i := 0
-
-	for _, passportRawRow := range passportRawData {
-		if doesPassportRawRowContainRequiredFields(passportRawRow, requiredFields) {
-			i++
-		}
-	}
-
-	return i
-}
-
-func doesPassportRawRowContainRequiredFields(passportRawRow string, requiredFields []string) bool {
 	for _, requiredField := range requiredFields {
 		matched, _ := regexp.Match(requiredField+":", []byte(passportRawRow))
 		if matched == false {
 			return false
 		}
+	}
+
+	return true
+}
+
+func validatorPt2(passportRawRow string) bool {
+	if isByrValid(getValue("byr", passportRawRow)) == false {
+		return false
+	}
+
+	if isIyrValid(getValue("iyr", passportRawRow)) == false {
+		return false
+	}
+
+	if isEyrValid(getValue("eyr", passportRawRow)) == false {
+		return false
+	}
+
+	if isHgtValid(getValue("hgt", passportRawRow)) == false {
+		return false
+	}
+
+	if isHclValid(getValue("hcl", passportRawRow)) == false {
+		return false
+	}
+
+	if isEclValid(getValue("ecl", passportRawRow)) == false {
+		return false
+	}
+
+	if isPidValid(getValue("pid", passportRawRow)) == false {
+		return false
+	}
+
+	return true
+}
+
+func getValue(key string, rawData string) string {
+	re := regexp.MustCompile(key + ":([0-9a-z#]+)")
+	matches := re.FindStringSubmatch(rawData)
+	if len(matches) == 0 {
+		return ""
+	}
+
+	return matches[1]
+}
+
+func isByrValid(byr string) bool {
+	return validateDate(1920, 2002, byr)
+}
+
+func isIyrValid(iyr string) bool {
+	return validateDate(2010, 2020, iyr)
+}
+
+func isEyrValid(Eyr string) bool {
+	return validateDate(2020, 2030, Eyr)
+}
+
+func isHgtValid(hgt string) bool {
+	re := regexp.MustCompile(`^(\d{3})cm$`)
+	matches := re.FindStringSubmatch(hgt)
+	if len(matches) > 0 {
+		hgtInCm, _ := strconv.Atoi(matches[1])
+		if hgtInCm > 193 {
+			return false
+		}
+		if hgtInCm < 150 {
+			return false
+		}
+
+		return true
+	}
+
+	re = regexp.MustCompile(`^(\d{2})in$`)
+	matches = re.FindStringSubmatch(hgt)
+	if len(matches) > 0 {
+		hgtInCm, _ := strconv.Atoi(matches[1])
+		if hgtInCm > 76 {
+			return false
+		}
+		if hgtInCm < 59 {
+			return false
+		}
+
+		return true
+	}
+
+	return false
+}
+
+func isHclValid(hcl string) bool {
+	return isRegexMatch(`^#[0-9a-f]{6}$`, hcl)
+}
+
+func isEclValid(hcl string) bool {
+	return isRegexMatch(`^amb|blu|brn|gry|grn|hzl|oth$`, hcl)
+}
+
+func isPidValid(pid string) bool {
+	return isRegexMatch(`^[0-9]{9}$`, pid)
+}
+
+func isRegexMatch(pattern string, s string) bool {
+	match, _ := regexp.MatchString(pattern, s)
+	return match
+}
+
+func validateDate(min int, max int, subject string) bool {
+	match, _ := regexp.MatchString(`^\d{4}$`, subject)
+	if match == false {
+		return false
+	}
+
+	value, _ := strconv.Atoi(subject)
+
+	if value < min {
+		return false
+	}
+
+	if value > max {
+		return false
 	}
 
 	return true
@@ -57,4 +186,6 @@ func loadFile() string {
 func main() {
 	fmt.Println("Pt1 No of Valid Passports")
 	fmt.Println(FindNoOfValidPassportsPt1(parseBatchFile(loadFile())))
+	fmt.Println("Pt2 No of Valid Passports")
+	fmt.Println(FindNoOfValidPassportsPt2(parseBatchFile(loadFile())))
 }
--- 04/04_test.go
+++ 04/04_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-var testInput string = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+var testInput = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
 byr:1937 iyr:2017 cid:147 hgt:183cm
 
 iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
@@ -18,10 +18,57 @@ hgt:179cm
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in`
 
+var testInputInvalid = `eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007`
+
+var testInputValid = `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`
+
 func TestFindNoOfValidPassportsPt1(t *testing.T) {
 	testData := parseBatchFile(testInput)
 	got := FindNoOfValidPassportsPt1(testData)
 	expected := 2
+
+	if got != expected {
+		t.Errorf("Expected %d, Got %d", expected, got)
+	}
+}
+
+func TestFindNoOfValidPassportsPt2Invalid(t *testing.T) {
+	testData := parseBatchFile(testInputInvalid)
+	got := FindNoOfValidPassportsPt2(testData)
+	expected := 0
+
+	if got != expected {
+		t.Errorf("Expected %d, Got %d", expected, got)
+	}
+}
+
+func TestFindNoOfValidPassportsPt2Valid(t *testing.T) {
+	testData := parseBatchFile(testInputValid)
+	got := FindNoOfValidPassportsPt2(testData)
+	expected := 4
 
 	if got != expected {
 		t.Errorf("Expected %d, Got %d", expected, got)
