19ce690952ca454c4ac40d724aaa3814ec307619
Jonathan Finch <thefinchmeister@gmail.com>
2020-12-25 21:35:52 +0000

    Add day 17, part 2

--- 17/17.go
+++ 17/17.go
@@ -8,23 +8,23 @@ import (
 
 func FindCubesInActiveStatePt1(cubes map[int]map[int]map[int]bool, cycles int) int {
 	for i := 0; i < cycles; i++ {
-		cubes = getNextState(cubes)
+		cubes = getNextStatePt1(cubes)
 	}
 
-	activeCubeCount := getActiveCubeCount(cubes)
+	activeCubeCount := getActiveCubeCountPt1(cubes)
 
 	return activeCubeCount
 }
 
-func getNextState(cubes map[int]map[int]map[int]bool) map[int]map[int]map[int]bool {
+func getNextStatePt1(cubes map[int]map[int]map[int]bool) map[int]map[int]map[int]bool {
 	nextCubes := make(map[int]map[int]map[int]bool)
-	expandedCubes := expandRange(cubes)
+	expandedCubes := expandRangePt1(cubes)
 	for z := range expandedCubes {
 		nextCubes[z] = make(map[int]map[int]bool)
 		for y := range expandedCubes[z] {
 			nextCubes[z][y] = make(map[int]bool)
 			for x := range expandedCubes[z][y] {
-				nextCubes[z][y][x] = getNextCubeState(x, y, z, cubes)
+				nextCubes[z][y][x] = getNextCubeStatePt1(x, y, z, cubes)
 			}
 		}
 	}
@@ -32,7 +32,7 @@ func getNextState(cubes map[int]map[int]map[int]bool) map[int]map[int]map[int]bo
 	return nextCubes
 }
 
-func expandRange(cubes map[int]map[int]map[int]bool) map[int]map[int]map[int]bool {
+func expandRangePt1(cubes map[int]map[int]map[int]bool) map[int]map[int]map[int]bool {
 
 	minX := 0
 	maxX := 0
@@ -86,7 +86,7 @@ func expandRange(cubes map[int]map[int]map[int]bool) map[int]map[int]map[int]boo
 	return expandedCubes
 }
 
-func getActiveCubeCount(cubes map[int]map[int]map[int]bool) int {
+func getActiveCubeCountPt1(cubes map[int]map[int]map[int]bool) int {
 	activeCubeCount := 0
 	for z := range cubes {
 		for y := range cubes[z] {
@@ -101,8 +101,8 @@ func getActiveCubeCount(cubes map[int]map[int]map[int]bool) int {
 	return activeCubeCount
 }
 
-func getNextCubeState(x int, y int, z int, cubes map[int]map[int]map[int]bool) bool {
-	activeNeighbours := countActiveNeighbours(x, y, z, cubes)
+func getNextCubeStatePt1(x int, y int, z int, cubes map[int]map[int]map[int]bool) bool {
+	activeNeighbours := countActiveNeighboursPt1(x, y, z, cubes)
 	isCubeActive := cubes[z][y][x] == true
 
 	if isCubeActive {
@@ -119,7 +119,7 @@ func getNextCubeState(x int, y int, z int, cubes map[int]map[int]map[int]bool) b
 	return false
 }
 
-func countActiveNeighbours(x int, y int, z int, cubes map[int]map[int]map[int]bool) int {
+func countActiveNeighboursPt1(x int, y int, z int, cubes map[int]map[int]map[int]bool) int {
 	activeNeighbours := 0
 
 	for zI := z - 1; zI <= z+1; zI++ {
@@ -138,7 +138,7 @@ func countActiveNeighbours(x int, y int, z int, cubes map[int]map[int]map[int]bo
 	return activeNeighbours
 }
 
-func parse(input string) map[int]map[int]map[int]bool {
+func parsePt1(input string) map[int]map[int]map[int]bool {
 	rows := strings.Split(input, "\n")
 	cubes := make(map[int]map[int]map[int]bool)
 	cubes[0] = make(map[int]map[int]bool)
@@ -148,6 +148,177 @@ func parse(input string) map[int]map[int]map[int]bool {
 			yRow[x] = cell == "#"
 		}
 		cubes[0][y] = yRow
+	}
+
+	return cubes
+}
+
+func FindCubesInActiveStatePt2(cubes map[int]map[int]map[int]map[int]bool, cycles int) int {
+	for i := 0; i < cycles; i++ {
+		cubes = getNextStatePt2(cubes)
+	}
+
+	activeCubeCount := getActiveCubeCountPt2(cubes)
+
+	return activeCubeCount
+}
+
+func getNextStatePt2(cubes map[int]map[int]map[int]map[int]bool) map[int]map[int]map[int]map[int]bool {
+	nextCubes := make(map[int]map[int]map[int]map[int]bool)
+	expandedCubes := expandRangePt2(cubes)
+
+	for w := range expandedCubes {
+		nextCubes[w] = make(map[int]map[int]map[int]bool)
+		for z := range expandedCubes[w] {
+			nextCubes[w][z] = make(map[int]map[int]bool)
+			for y := range expandedCubes[w][z] {
+				nextCubes[w][z][y] = make(map[int]bool)
+				for x := range expandedCubes[w][z][y] {
+					nextCubes[w][z][y][x] = getNextCubeStatePt2(x, y, z, w, cubes)
+				}
+			}
+		}
+	}
+
+	return nextCubes
+}
+
+func expandRangePt2(cubes map[int]map[int]map[int]map[int]bool) map[int]map[int]map[int]map[int]bool {
+
+	minX := 0
+	maxX := 0
+	minY := 0
+	maxY := 0
+	minZ := 0
+	maxZ := 0
+	minW := 0
+	maxW := 0
+
+	for w := range cubes {
+		if w > maxW {
+			maxW = w
+		}
+		if w < minW {
+			minW = w
+		}
+	}
+
+	for z := range cubes[0] {
+		if z > maxZ {
+			maxZ = z
+		}
+		if z < minZ {
+			minZ = z
+		}
+	}
+
+	for y := range cubes[0][0] {
+		if y > maxY {
+			maxY = y
+		}
+		if y < minY {
+			minY = y
+		}
+	}
+
+	for x := range cubes[0][0][0] {
+		if x > maxX {
+			maxX = x
+		}
+		if x < minX {
+			minX = x
+		}
+	}
+
+	expandedCubes := make(map[int]map[int]map[int]map[int]bool)
+
+	for w := minW - 1; w <= maxW+1; w++ {
+		expandedCubes[w] = make(map[int]map[int]map[int]bool)
+		for z := minZ - 1; z <= maxZ+1; z++ {
+			expandedCubes[w][z] = make(map[int]map[int]bool)
+			for y := minY - 1; y <= maxY+1; y++ {
+				expandedCubes[w][z][y] = make(map[int]bool)
+				for x := minX - 1; x <= maxX+1; x++ {
+					if cubes[w][z][y][x] == true {
+						expandedCubes[w][z][y][x] = true
+						continue
+					}
+					expandedCubes[w][z][y][x] = false
+				}
+			}
+		}
+	}
+
+	return expandedCubes
+}
+
+func getActiveCubeCountPt2(cubes map[int]map[int]map[int]map[int]bool) int {
+	activeCubeCount := 0
+	for w := range cubes {
+		for z := range cubes[w] {
+			for y := range cubes[w][z] {
+				for x := range cubes[w][z][y] {
+					if cubes[w][z][y][x] {
+						activeCubeCount++
+					}
+				}
+			}
+		}
+	}
+
+	return activeCubeCount
+}
+
+func getNextCubeStatePt2(x int, y int, z int, w int, cubes map[int]map[int]map[int]map[int]bool) bool {
+	activeNeighbours := countActiveNeighboursPt2(x, y, z, w, cubes)
+	isCubeActive := cubes[w][z][y][x] == true
+
+	if isCubeActive {
+		if activeNeighbours == 2 || activeNeighbours == 3 {
+			return true
+		}
+		return false
+	}
+
+	if activeNeighbours == 3 {
+		return true
+	}
+
+	return false
+}
+
+func countActiveNeighboursPt2(x int, y int, z int, w int, cubes map[int]map[int]map[int]map[int]bool) int {
+	activeNeighbours := 0
+
+	for wI := w - 1; wI <= w+1; wI++ {
+		for zI := z - 1; zI <= z+1; zI++ {
+			for yI := y - 1; yI <= y+1; yI++ {
+				for xI := x - 1; xI <= x+1; xI++ {
+					if xI == x && yI == y && zI == z && wI == w {
+						continue
+					}
+					if cubes[wI][zI][yI][xI] == true {
+						activeNeighbours++
+					}
+				}
+			}
+		}
+	}
+
+	return activeNeighbours
+}
+
+func parsePt2(input string) map[int]map[int]map[int]map[int]bool {
+	rows := strings.Split(input, "\n")
+	cubes := make(map[int]map[int]map[int]map[int]bool)
+	cubes[0] = make(map[int]map[int]map[int]bool)
+	cubes[0][0] = make(map[int]map[int]bool)
+	for y, row := range rows {
+		yRow := make(map[int]bool)
+		for x, cell := range strings.Split(row, "") {
+			yRow[x] = cell == "#"
+		}
+		cubes[0][0][y] = yRow
 	}
 
 	return cubes
@@ -164,5 +335,7 @@ func loadFile() string {
 
 func main() {
 	fmt.Println("Pt1")
-	fmt.Println(FindCubesInActiveStatePt1(parse(loadFile()), 6))
+	fmt.Println(FindCubesInActiveStatePt1(parsePt1(loadFile()), 6))
+	fmt.Println("Pt2")
+	fmt.Println(FindCubesInActiveStatePt2(parsePt2(loadFile()), 6))
 }
--- 17/17_test.go
+++ 17/17_test.go
@@ -9,8 +9,17 @@ var testInput = `.#.
 ###`
 
 func TestFindCubesInActiveStatePt1(t *testing.T) {
-	got := FindCubesInActiveStatePt1(parse(testInput), 6)
+	got := FindCubesInActiveStatePt1(parsePt1(testInput), 6)
 	expected := 112
+
+	if got != expected {
+		t.Errorf("Expected %d, got %d", expected, got)
+	}
+}
+
+func TestFindCubesInActiveStatePt2(t *testing.T) {
+	got := FindCubesInActiveStatePt2(parsePt2(testInput), 6)
+	expected := 848
 
 	if got != expected {
 		t.Errorf("Expected %d, got %d", expected, got)
